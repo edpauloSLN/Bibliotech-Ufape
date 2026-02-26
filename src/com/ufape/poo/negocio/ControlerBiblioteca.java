1e4e1302b4716afb47c57f43066a330a37f54280
edpauloSLN <edpauloch@gmail.com>
2026-02-26 15:54:42 -0300

    Update ControlerBiblioteca.java

--- src/com/ufape/poo/negocio/ControlerBiblioteca.java
+++ src/com/ufape/poo/negocio/ControlerBiblioteca.java
@@ -2,4 +2,29 @@ package com.ufape.poo.negocio;
 
 public class ControlerBiblioteca {
 
+    private UsuarioService usuarioService;
+    private static ControlerBiblioteca instance;
+
+    private ControlerBiblioteca() {
+        this.usuarioService = new UsuarioService(); 
+    }
+
+    public static ControlerBiblioteca getInstance() {
+        if (instance == null) {
+            instance = new ControlerBiblioteca();
+        }
+        return instance;
+    }
+
+    public void cadastrarUsuario(Usuario u) throws UsuarioJaExisteException {
+        usuarioService.cadastrar(u);
+    }
+
+    public Usuario buscarUsuario(String matricula) throws UsuarioNaoEncontradoException {
+        return usuarioService.buscar(matricula);
+    }
+    
+    public void bloquearUsuario(String matricula) throws UsuarioNaoEncontradoException, OperacaoNaoPermitidaException {
+        usuarioService.bloquear(matricula);
+    }
 }
